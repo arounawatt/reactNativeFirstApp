a8c0261d9011d9a5406b3b2e0f35f37ec6801d66
arounawatt <31615132+arounawatt@users.noreply.github.com>
2022-02-03 18:03:54 +0100

    First commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import BookScreen from './screens/BookScreen';
+
+const App = () => {
+  return (
+    <BookScreen />
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
